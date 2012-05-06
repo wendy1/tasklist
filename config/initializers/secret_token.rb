6babea748051e8c8dabed7629e931bb31da703c7
Wendy Swanbeck <olive.swanbeck@verizon.net>
2012-05-06 10:20:13 -0400

    Initial generate files (minus index.html)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasklist::Application.config.secret_token = 'ca790691c1a646d98751e2a014c8e5457a44937ef4d66b074c3e5398de3ccdb16d259de6c886ef7557a8d292ea60adebed18dcfd76d19687dd008c00db35c4a6'
