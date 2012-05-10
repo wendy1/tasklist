1869e8ba384c8f4a4ad26d330b688ee33713be82
Wendy Swanbeck <olive.swanbeck@verizon.net>
2012-05-09 22:45:22 -0400

    add subtask works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,19 +3,25 @@ class TasksController < ApplicationController
   def index
     @title = "Task list"
     @tasks = Task.find :all, :conditions => 'id NOT IN (select distinct child_id from relationships)'
-    Rails::logger.info "------------------ @task ids are"
-    @tasks.each do |t|; Rails::logger.info t.id.to_s; end
-    Rails::logger.info "--------------------"
   end
   
   def new
     @title = "New task"
-    @task = Task.new
+    if (params[:parent_id] != nil)
+      @task = Task.find_by_id(params[:parent_id]).subtasks.new
+    else
+      @task = Task.new
+    end
   end
   
   def create
-    @task = Task.new(params[:task])
-    @task.save
+    parent = params[:parent_id]
+    Rails::logger.info "========= parent="+ (parent.nil? ? "nil" :  parent) 
+    unless parent.nil?
+      @task = Task.find_by_id(parent).subtasks.create(params[:task])
+    else
+      @task = Task.create(params[:task])
+    end
     redirect_to root_path
   end
 
